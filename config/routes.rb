fe27a0bd8c995f9048cb46fbae5826cd5df2e3f9
Camron Khan <camron.khan@gmail.com>
2016-06-04 13:17:01 -0500

    added blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-    root 'static_pages#home'
-    
-    get 'static_pages/home'
-    get 'static_pages/about'
-    get 'static_pages/education'
-    get 'static_pages/experience'
-    
+  root 'static_pages#home'
+  
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/education'
+  get 'static_pages/experience'
+  
+  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
